b84d2b7418c39ce61337d584dd7a030bbeaf5d5c
nathanielsebastian <nathanielsebastian0@gmail.com>
2023-07-18 18:08:46 +0700

    Parsing Data Passport

--- src/functions.js
+++ src/functions.js
@@ -1,48 +1,48 @@
 // const { createWorker, PSM} = Tesseract;
 var promises_1 = require("fs").promises;
 const { log } = require("console");
-const { createWorker } = require('tesseract.js');
-const parse = require('mrz').parse;
+const { createWorker } = require("tesseract.js");
+const parse = require("mrz").parse;
 
-const progress = document.getElementById('progress')
-const textarea = document.getElementById('textarea')
-const canvas = document.getElementById("cv1")
-const canvas2 = document.getElementById("cv2")
+const progress = document.getElementById("progress");
+const textarea = document.getElementById("textarea");
+const canvas = document.getElementById("cv1");
+const canvas2 = document.getElementById("cv2");
 const ctx = cv1.getContext("2d");
 const ctx2 = cv2.getContext("2d");
 
-document.querySelector('input[type="file"]').onchange = function() {
-    let img = this.files[0]
-    let reader = new FileReader()
-    reader.readAsDataURL(img)
-    reader.onload = function() {
-      drawImage(reader.result)
-    }
-}
+document.querySelector('input[type="file"]').onchange = function () {
+  let img = this.files[0];
+  let reader = new FileReader();
+  reader.readAsDataURL(img);
+  reader.onload = function () {
+    drawImage(reader.result);
+  };
+};
 
 function drawImage(url) {
-  let image = new Image()
-  image.src = url
+  let image = new Image();
+  image.src = url;
   image.onload = () => {
-      canvas.width = image.width
-      canvas.height = image.height
-      ctx.drawImage(image, 0, 0)
-      const dataURI = canvas.toDataURL('image/jpeg');
+    canvas.width = image.width;
+    canvas.height = image.height;
+    ctx.drawImage(image, 0, 0);
+    const dataURI = canvas.toDataURL("image/jpeg");
 
-      canvas2.width = image.width
-      canvas2.height = image.height
-      ctx2.drawImage(image, 0, 0)
-      const dataURL = canvas2.toDataURL('image/jpeg');
+    canvas2.width = image.width;
+    canvas2.height = image.height;
+    ctx2.drawImage(image, 0, 0);
+    const dataURL = canvas2.toDataURL("image/jpeg");
 
-      preprocessImage(canvas2);
-      scanImg(dataURL, 'spa+ces');
-  }
+    preprocessImage(canvas2);
+    scanImg(dataURL, "spa+ces");
+  };
 }
 
-async function scanImg(src,lang){
+async function scanImg(src, lang) {
   const dtTxt = [];
   // Tesseract.recognize(
-  //   src, 
+  //   src,
   //   lang, {
   //   tessedit_char_whitelist:
   //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<",
@@ -76,84 +76,98 @@ async function scanImg(src,lang){
 
   //use worker
 
-    const worker = await createWorker({});
-    await worker.loadLanguage(lang); // 2
-    await worker.initialize(lang);
-    await worker.setParameters({
-      tessedit_char_whitelist:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<",
-      preserve_interword_spaces: '10',
-    })
-    const {
-      data: { text, words },
-    } = await worker.recognize(src);
-    await worker.terminate();
+  const worker = await createWorker({});
+  await worker.loadLanguage(lang); // 2
+  await worker.initialize(lang);
+  await worker.setParameters({
+    tessedit_char_whitelist:
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<",
+    preserve_interword_spaces: "10",
+  });
+  const {
+    data: { text, words },
+  } = await worker.recognize(src);
+  await worker.terminate();
 
-    const textRegions = words.map((word) => word.bbox);
-    ctx2.lineWidth = 2;
-    ctx2.strokeStyle = "red";
-    textRegions.forEach((region) => {
-      ctx2.beginPath();
-      ctx2.rect(
-        region.x0,
-        region.y0,
-        region.x1 - region.x0,
-        region.y1 - region.y0
-      );
-      ctx2.stroke();
-    });
+  const textRegions = words.map((word) => word.bbox);
+  ctx2.lineWidth = 2;
+  ctx2.strokeStyle = "red";
+  textRegions.forEach((region) => {
+    ctx2.beginPath();
+    ctx2.rect(
+      region.x0,
+      region.y0,
+      region.x1 - region.x0,
+      region.y1 - region.y0
+    );
+    ctx2.stroke();
+  });
 
-    const textVal = words.map((word) => word.text);
-    textVal.forEach((txt) => {
-      dtTxt.push(txt)
-    });
+  const textVal = words.map((word) => word.text);
+  textVal.forEach((txt) => {
+    dtTxt.push(txt);
+  });
 
-    parseMRZ(text);
+  parseMRZ(text);
 }
 
-function parseMRZ(mrzTxt){
+function parseMRZ(mrzTxt) {
   //Array for RAW parse result
-  const mrzArr= [];
+  const mrzArr = [];
   //Array for mapped parse result
   const ressArr = [
     {
       field: "",
-      val: ""
-    }
+      val: "",
+    },
   ];
 
   const lines = mrzTxt.split("\n");
   const lastTwoLines = lines.slice(-3);
   const mergedText = lastTwoLines.join("\n");
 
-  const mrz = mergedText.split("\n")
+  const mrz = mergedText.split("\n");
   const line1 = mrz[0];
   const line2 = mrz[1];
 
   mrzArr.push(line1, line2);
   var result = parse(mrzArr);
 
-  const mrzRes = result.details.map(dt => ({ field: dt.field, value: dt.value }));
-  ressArr.push(mrzRes)
+  const mrzRes = result.details.map((dt) => dt.value);
+  ressArr.push(mrzRes);
 
   console.log(mrzRes);
-  const stringed = JSON.stringify(mrzRes)
-  textarea.innerHTML = stringed
+  const stringed = JSON.stringify(mrzRes);
+  // console.log(stringed);
+  textarea.innerHTML = 
+  "MRZ Scanner = " + "\n" + line1 + "\n" + line2 + "\n" + "\n" +
+  "Document Code = " + mrzRes[0] + "\n" + 
+  "Issuing State = " + mrzRes[1] + "\n" + 
+  "Last Name = " + mrzRes[2] + "\n" +
+  "First Name = " + mrzRes[3] + "\n" +
+  "Document Number = " + mrzRes[4] + "\n" +
+  "Document NumberCheckDigit = " + mrzRes[5] + "\n" +
+  "Nationality = " + mrzRes[6] + "\n" +
+  "BirthDate = " + mrzRes[7] + "\n" +
+  "BirthDate CheckDigit = " + mrzRes[8] + "\n" +
+  "Sex = " + mrzRes[9] + "\n" +
+  "Expiration Date = " + mrzRes[10] + "\n" +
+  "Expiration DateCheckDigit = " + mrzRes[11] + "\n" +
+  "Personal Number = " + mrzRes[12] + "\n" +
+  "Personal Number CheckDigit = " + mrzRes[13] + "\n" +
+  "Composite CheckDigit = " + mrzRes[14];
+  
 }
 
 function preprocessImage(canvas) {
-  convertToGrayscale(canvas)
-  increaseContrast(canvas)
+  convertToGrayscale(canvas);
+  increaseContrast(canvas);
 }
 
-function convertToGrayscale(cv){
+function convertToGrayscale(cv) {
   //Convert Img to Grayscale
-  const imageData = cv.getContext("2d").getImageData(
-    0,
-    0,
-    cv.width,
-    cv.height
-  );
-  
+  const imageData = cv.getContext("2d").getImageData(0, 0, cv.width, cv.height);
+
   const imgData = imageData.data;
   for (let i = 0; i < imgData.length; i += 4) {
     const avg = (imgData[i] + imgData[i + 1] + imgData[i + 2]) / 3;
@@ -162,21 +176,15 @@ function convertToGrayscale(cv){
     imgData[i + 2] = avg; // B
   }
   cv.getContext("2d").putImageData(imageData, 0, 0);
-  console.log(imageData)
 }
 
-function increaseContrast(cv){
-  const cvDt = cv.getContext("2d")
-  const imageData = cvDt.getImageData(
-    0,
-    0,
-    cv.width,
-    cv.height
-  );
-  
+function increaseContrast(cv) {
+  const cvDt = cv.getContext("2d");
+  const imageData = cvDt.getImageData(0, 0, cv.width, cv.height);
+
   const imgData = imageData.data;
-   // Inncrease contrast
-   for (let i = 0; i < imgData.length; i += 4) {
+  // Inncrease contrast
+  for (let i = 0; i < imgData.length; i += 4) {
     if (imgData[i] < 124) {
       // Can be adjusted
       imgData[i] = 0; // R
@@ -189,33 +197,4 @@ function increaseContrast(cv){
     }
   }
   cvDt.putImageData(imageData, 0, 0);
-}
-
-function checkMRZ(img, lang){
-  (async () => {
-    const worker = await createWorker({
-      logger: (m) => {
-        console.log(m);
-      },
-    });
-    await worker.loadLanguage(lang); // 2
-    await worker.initialize(lang);
-    await worker.setParameters({
-      preserve_interword_spaces: '10',
-      tessedit_char_whitelist: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789<",
-    })
-    const {
-      data: { text },
-    } = await worker.recognize(img);
-    const detectedText = text;
-
-    // Mengambil 2 baris terakhir
-    const lines = detectedText.split("\n");
-    const lastTwoLines = lines.slice(-3);
-    const mergedText = lastTwoLines.join("\n");
-
-    console.log(mergedText);
-
-    await worker.terminate();
-  })();
 }
